b1df39524f47096d687cb9c60c04c9635c5e3af8
alpha <dentalon599@gmail.com>
2019-04-27 13:22:31 +0300

    Working chat

--- main.go
+++ main.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 	"log"
 	"net/http"
 
+	_ "github.com/go-sql-driver/mysql"
 	"github.com/gorilla/mux"
 	"github.com/gorilla/websocket"
 )
@@ -16,7 +18,14 @@ var upgrader = websocket.Upgrader{
 		return true
 	},
 }
+
 var clients []*websocket.Conn
+
+type message struct {
+	id_message  int
+	author_name string
+	text        string
+}
 
 func SocketFunc(w http.ResponseWriter, r *http.Request) {
 	ws, err := upgrader.Upgrade(w, r, nil)
@@ -25,13 +34,39 @@ func SocketFunc(w http.ResponseWriter, r *http.Request) {
 	}
 	// defer ws.Close()
 	clients = append(clients, ws)
+	// db, err := sql.Open("mysql", "chat:123456@tcp(192.168.43.245:3306)/chat")
+	// if err != nil {
+	// 	fmt.Println("Database error", err)
+	// }
+	// defer db.Close()
+	// msgs, err := db.Query("SELECT * FROM Messages")
+	// if err != nil {
+	// 	panic(err)
+	// }
+	// defer msgs.Close()
+	// messages := []message{}
+
+	// for msgs.Next() {
+	// 	msg := message{}
+	// 	err := msgs.Scan(&msg.id_message, &msg.author_name, &msg.text)
+	// 	if err != nil {
+	// 		fmt.Println("Error reading from db!", err)
+	// 		continue
+	// 	}
+	// 	messages = append(messages, msg)
+	// }
+	// for _, message := range messages {
+	// 	fmt.Printf("id: %d, author: %s, text: %s\n", message.id_message, message.author_name, message.text)
+	// }
+	fmt.Println("Ws:", ws)
 	go ReadSocket(ws)
+
 }
 
 func ReadSocket(ws *websocket.Conn) {
-	fmt.Println("TEST")
+	fmt.Println("State of clients:")
+	fmt.Println(len(clients))
 	for {
-		// _, message, err := ws.ReadMessage()
 		type message struct {
 			Text   string `json:"text"`
 			Author string `json:"author"`
@@ -39,12 +74,29 @@ func ReadSocket(ws *websocket.Conn) {
 		var msg message
 		err := websocket.ReadJSON(ws, &msg)
 		if err != nil {
+			// _, _, err := ws.ReadMessage()
+			// if err != nil {
 			fmt.Println("Disconnect")
+			CloseAndRemove(ws)
 			// ws.Close()
+
+			// }
+			fmt.Println("Client was disconnected")
 			break
 		}
 		fmt.Println("Message: ", msg.Text)
 		fmt.Println("Author: ", msg.Author)
+		db, err := sql.Open("mysql", "chat:123456@tcp(192.168.43.245:3306)/chat")
+		if err != nil {
+			fmt.Println("Database error", err)
+		}
+		defer db.Close()
+		insert, err := db.Query("INSERT INTO Messages (author_name, text) VALUES (?, ?)", msg.Author, msg.Text)
+		if err != nil {
+			fmt.Println("Database insert error", err)
+			break
+		}
+		insert.Close() // Correct closing
 		for _, client := range clients {
 			if err := websocket.WriteJSON(client, msg); err != nil {
 				fmt.Println("Disconnect")
@@ -56,6 +108,21 @@ func ReadSocket(ws *websocket.Conn) {
 	}
 }
 
+func CloseAndRemove(ws *websocket.Conn) {
+	index := -1
+	defer ws.Close()
+	for i, socket := range clients {
+		if socket == ws {
+			index = i
+			break
+		}
+	}
+	if index == -1 {
+		fmt.Println("Socket not found!")
+		return
+	}
+	clients = append(clients[:index], clients[index+1:]...)
+}
 func MainFunc(w http.ResponseWriter, r *http.Request) {
 	// vars := mux.Vars(r)
 	// id := vars["id"]
